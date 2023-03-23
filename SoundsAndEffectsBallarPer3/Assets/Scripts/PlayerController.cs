8677ed5b116ef3b0c84af6b594af499b5216c246
Julian0330 <744251@student.dusd.net>
2023-03-23 11:40:04 -0700

    Started Project
    Started project and almost finished 3.1

---
+++ SoundsAndEffectsBallarPer3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float jumpForce;
+    public float gravityModifier;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        playerRb = GetComponent<Rigidbody>();
+        playerRb.AddForce(Vector3.up * 1000);
+    }
+    public bool isOnGround = true;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
+    }
+}
+
